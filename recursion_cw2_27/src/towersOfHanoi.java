d415195b0142d3765eff8242d7fd6a0889a39598
tcossoul <tcossoul@users.noreply.github.com>
2019-03-03 14:20:26 -0800

    adding data directory

---
+++ recursion_cw2_27/src/towersOfHanoi.java
@@ -0,0 +1,9 @@
+//public class towersOfHanoi {
+//    public static void displaySolution( int N) {
+//        if (N is too big or too small then ask again){
+//          check if there is a move you can make where you move a disk on top of another one
+//          else{
+//         randomly choose a valid move ( ontop of the smallest disk that is still larger than the one you are trying to move
+//        }
+//    }
+//}
