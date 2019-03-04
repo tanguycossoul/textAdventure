0798230693bf0a13d90a6d9b40969d324b8b9aec
tcossoul <tcossoul@users.noreply.github.com>
2019-03-03 18:29:59 -0800

    cleanup

--- recursion_cw2_27/src/towersOfHanoi.java
+++ recursion_cw2_27/src/towersOfHanoi.java
@@ -1,3 +1,14 @@
+/*
+    Principle:
+        - 3 rods and a number of disks of different sizes, which can slide onto any rod.
+        - starts with the disks in a neat stack in ascending order of size on one rod, the smallest at the top
+        - objective is to move the entire stack to another rod, obeying the following rules:
+            - Only one disk can be moved at a time.
+            - Each move consists of taking the upper disk from one of the stacks and placing it on top of another stack.
+            - No disk may be placed on top of a smaller disk.
+        - The minimal number of moves required is 2n − 1, where n is the number of disks.
+ */
+
 //public class towersOfHanoi {
 //    public static void displaySolution( int N) {
 //        if (N is too big or too small then ask again){
